9d74a9d687f50c12393f5676324c14a5a14ce348
essiviippola <essi.viippola@live.fi>
2022-01-12 12:23:15 +0200

    Add vaccination data paths

--- finregistry_data/config.py
+++ finregistry_data/config.py
@@ -1,3 +1,10 @@
 from pathlib import Path
 
 ROOT_DIR = Path("/data")
+ORIGINAL_DATA_DIR = ROOT_DIR / "original_data"
+
+THL_VACCINATION_DIR = ORIGINAL_DATA_DIR / "thl_vaccination"
+VACCINATION_PROTECTION_PATH = THL_VACCINATION_DIR / "thl2196_rokotussuoja.csv"
+VACCINATION_REGISTRY_PATH = (
+    THL_VACCINATION_DIR / "thl2196_rokoterekisteri.csv.finreg_IDs"
+)
