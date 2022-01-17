02de97d6ace4982fbb53bb6f849662bee0e8ad54
essiviippola <essi.viippola@live.fi>
2022-01-17 08:36:45 +0200

    Replace missing values with NA

--- finregistry_data/thl_infectious_diseases.py
+++ finregistry_data/thl_infectious_diseases.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from config import THL_INFECTIOUS_DISEASES_DATA_PATH
 
+MISSING_VALUES = [0]
+MISSING_VALUES_STR = [str(i) for i in MISSING_VALUES]
+
 
 def read_data(filepath=THL_INFECTIOUS_DISEASES_DATA_PATH):
     """Read infectious diseases data"""
@@ -13,5 +16,12 @@ def read_data(filepath=THL_INFECTIOUS_DISEASES_DATA_PATH):
         "ARVO_TEKSTI": str,
     }
     df = pd.read_csv(filepath, sep=";", encoding="latin9", dtype=dtypes,)
+    return df
+
+
+def replace_missing_with_na(df):
+    """Replace missing vaues with NA"""
+    d = {"ARVO_KOODI": MISSING_VALUES, "ARVO_TEKSTI": MISSING_VALUES_STR}
+    df = df.replace(d, pd.NA)
     return df
 
