4a9053d20e5fb58b393cf7ee2fa7f495351ce168
jascha1777 <97315852+jascha1777@users.noreply.github.com>
2022-06-17 08:28:04 +0200

    Create pno_harmonization.py
    Harmonization of Postal Codes and Municipalities by converting them to 2022 versions

---
+++ finregistry_data/dvv_extended/pno_harmonization.py
@@ -0,0 +1,14 @@
+'''Harmonization of Postal Codes and Municipalities by converting them to 2022 versions'''
+import geopandas
+import pandas
+
+pno = geopandas.read_file('/data/projects/project_akarvane/geo/pno_2022/pno_2022Polygon.shp')  # 2022 Postal Codes and Municipalities
+living = pandas.read_csv('/data/processed_data/dvv/Tulokset_1900-2010_tutkhenk_asuinhist.txt.finreg_IDsp')
+
+living = living.head(10)
+
+geo_living = geopandas.GeoDataFrame(living, geometry=geopandas.points_from_xy(living.Longitude, living.Latitude, crs='epsg:3067'))
+
+living_pno = geo_living.sjoin(pno, how='left', predicate='intersects')
+
+living_pno.to_csv('/data/projects/project_akarvane/geo/living_pno.csv')
