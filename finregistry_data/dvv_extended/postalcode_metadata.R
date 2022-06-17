f1f4038d050744ec3b1bbddd171992e143989a4d
jascha1777 <97315852+jascha1777@users.noreply.github.com>
2022-06-17 09:07:34 +0200

    Update and rename dvv_core.R to postalcode_metadata.R

--- finregistry_data/dvv_extended/postalcode_metadata.R
+++ finregistry_data/dvv_extended/postalcode_metadata.R
@@ -9,10 +9,7 @@ library(data.table)
 library(R.utils)
 
 # Reading the DVV file
-geo_data <- fread("/data/projects/project_akarvane/geo/living_pno.csv")
-
-# Removing duplicates
-geo_data <- geo_data %>% distinct(FINREGISTRYID, Start_of_residence, .keep_all = TRUE)
+geo_data <- fread("~/dvv_ext_core.csv")
 
 # Reading the Postal Codes Metadata (locally processed; find source in Documentation)
 paavo_2013 <- fread("sotka_paavo/paavo_2013.csv") 
@@ -23,9 +20,9 @@ geo_paavo <- geo_data %>% left_join(paavo_2013, by = c("posti_alue" = "postal_co
 # Keep only columns with missing values < 20%
 geo_paavo <- select(geo_paavo, which(colMeans(is.na(geo_paavo))< 0.2))
 
-# Removing unuseful columns
-geo_paavo <- geo_paavo %>% select(-Residence_type, -Municipality, -Municipality_name, -V1,
-                                       -Building_ID, -Residence_code, -Post_code)
+geo_paavo <- geo_paavo %>% select("ko_perus":"ident")
+geo_paavo <- geo_paavo %>% select(ident, everything())
+
 
 # Write dvv_ext_postal_codes.csv
 fwrite(geo_paavo, "dvv_ext_postal_codes.csv")
