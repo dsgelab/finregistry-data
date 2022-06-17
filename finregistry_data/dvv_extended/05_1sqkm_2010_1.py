3242f36fd4d151eae5628b753f95a6f142298288
jascha1777 <97315852+jascha1777@users.noreply.github.com>
2022-06-17 09:21:31 +0200

    Update 05_1sqkm_2010_1.py

--- finregistry_data/dvv_extended/05_1sqkm_2010_1.py
+++ finregistry_data/dvv_extended/05_1sqkm_2010_1.py
@@ -5,13 +5,11 @@ import geopandas
 shp2010 = geopandas.read_file("one_km_files/rttk1km_2010.shp")
 shp2010.head()
 
-dvv = pandas.read_csv('/data/projects/project_akarvane/geo/living_pno.csv')
+dvv = pandas.read_csv('~/dvv_ext_core.csv')
 dvv.columns
-dvv = dvv.drop(['Residence_type', 'Municipality', 'Municipality_name', 'Unnamed: 0', 'Building_ID', 'Residence_code', 'Post_code'], axis=1)
 
-dvv = dvv.drop_duplicates(['FINREGISTRYID','Start_of_residence'])  # *1
+dvv = dvv.drop_duplicates(['FINREGISTRYID','Start_of_residence'])  
 dvv.shape  # must be: 35041489 , 15
-#s1 = dvv.iloc[:, : 15]        # if ncol of dvv = 15, then no subset needed, because it's not a subset then
 
 geo_s1 = geopandas.GeoDataFrame(dvv, geometry=geopandas.points_from_xy(dvv.Longitude, dvv.Latitude, crs='epsg:3067'))
 geo_s1.rename(columns = {'index_right':'index_right_0'}, inplace = True)
